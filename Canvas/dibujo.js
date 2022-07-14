68e7d338e820b0c736f541f68f4372c941aefbac
Alexisliao10 <Alexisliao10@hotmail.com>
2022-07-14 10:55:07 -0500

    canvas dibujo basico con multiplicacion

--- Canvas/dibujo.js
+++ Canvas/dibujo.js
@@ -1,5 +1,31 @@
 var d = document.getElementById("dibujito");
 var lienzo = d.getContext("2d")
+var linea = 30
+var l = 0;
+var yi, xf;
+var colores = "blue";
+
+dibujarlinea(colores, 1, 1, 1, 300)
+dibujarlinea(colores, 300, 300, 300, 1)
+dibujarlinea(colores, 1, 299, 299, 299)
+dibujarlinea(colores, 299, 1, 1, 1)
+
+for(l = 0; l < linea; l++)
+{
+    yi = 10 * l;
+    xf = 10 * (l + 1);
+    dibujarlinea(colores, 0, yi, xf, 300);
+    console.log("linea " + l);
+}
+
+for(l = 0; l < linea; l++)
+{
+    yi = 10 * l;
+    xf = 10 * (l + 1);
+    dibujarlinea(colores, 300, yi, xf, 0);
+    console.log("linea " + l);
+}
+
 
 function dibujarlinea(color, xinicial, yinicial, xfinal, yfinal)
 {
@@ -10,7 +36,3 @@ function dibujarlinea(color, xinicial, yinicial, xfinal, yfinal)
     lienzo.stroke();
     lienzo.closePath();   
 }
-
-dibujarlinea("pink", 10, 300, 220, 10);
-dibujarlinea("red", 300, 10, 10, 220);
-s
